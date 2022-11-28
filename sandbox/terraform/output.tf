987ebba371615f69c9b995ebf843c2d659976545
Peter Eskandar <peter.eskandar@posteitaliane.it>
2022-11-28 16:25:16 +0100

    test org data types

--- sandbox/terraform/output.tf
+++ sandbox/terraform/output.tf
@@ -4,6 +4,11 @@ output "current_org" {
   value = data.aws_organizations_organization.current_org
 }
 
+output "account_info" {
+  value = data.aws_caller_identity.current
+}
+
+
 /*output "account_ids" {
   value = data.aws_organizations_organization.current_org.accounts[*].id
 }*/
