8fb41eb989e23480c2f6192c8604cfb6f4f8e5e1
Peter Eskandar <peter.eskandar@posteitaliane.it>
2022-11-28 14:32:07 +0100

    update current_org

--- sandbox/terraform/output.tf
+++ sandbox/terraform/output.tf
@@ -1,4 +1,9 @@
 # output list of account_ids 
+
+output "current_org" {
+  value = data.aws_organizations_organization.current_org
+}
+
 output "account_ids" {
   value = data.aws_organizations_organization.current_org.accounts[*].id
 }
